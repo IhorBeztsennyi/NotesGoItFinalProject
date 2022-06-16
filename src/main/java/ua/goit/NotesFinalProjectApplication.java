9f10b5530c19538caf5a90f814dcdb05546afe4a
IhorBeztsennyi <coalenergy84@ukr.net>
2022-06-16 12:20:54 +0300

    Init commit

---
+++ src/main/java/ua/goit/NotesFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.goit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/goit/NotesFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.goit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
