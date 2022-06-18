8fc3f0d47c3c3753f8338d1f59385f85fadf198c
IhorBeztsennyi <coalenergy84@ukr.net>
2022-06-19 00:27:30 +0300

    Many to one relation, note table, note converter and note repository was added.

--- src/main/java/ua/goit/users/UserController.java
+++ src/main/java/ua/goit/users/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
