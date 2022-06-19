0948ae148ba36a385a3c8f1530636135881259bc
Poltava <quazantip@gmail.com>
2022-06-20 00:14:35 +0400

    :bookmark: Note Controller filled with methods( looks primitive); Note Repository find by name method made return 1 object; Note Service all required method added with Pagination;

--- src/main/java/ua/goit/note/NoteController.java
+++ src/main/java/ua/goit/note/NoteController.java
@@ -1,13 +1,16 @@
 package ua.goit.note;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/note/list")
 public class NoteController {
 
     private final NoteService noteService;
@@ -17,8 +20,24 @@ public class NoteController {
         this.noteService = noteService;
     }
 
-    public List<NoteDto> getNotes(){
-        return null;
+    public Page<NoteDto> getNotes(@PageableDefault(sort = "id", direction= Sort.Direction.DESC,size = 5) Pageable p){
+        return noteService.viewNotes(p);
+    }
+
+    public void saveNote(NoteDto n){
+        noteService.createNote(n);
+    }
+
+    public void updateNote(NoteDto note){
+        noteService.editNote(note);
+    }
+
+    public void deleteNote(@RequestParam UUID id){
+        noteService.deleteNoteById(id);
+    }
+
+    public NoteDto findNoteByName(String name){
+        return noteService.findByName(name);
     }
 
 }
