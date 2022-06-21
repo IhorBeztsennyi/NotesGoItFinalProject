841f8613e30b9064a4ee707407a718052dedd020
IhorBeztsennyi <coalenergy84@ukr.net>
2022-06-21 16:56:22 +0300

    added admin by V2__Create_admin.sql

---
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, email, password, user_role, username)
+VALUES
+    ('7d7e6e35-f7c9-40a1-9efd-a1c5c963d919', 'admin@gmail.com','$2a$12$Ymy2NsY2mm72faISck74Uesp0nRRHwm/LkmFuCU7A8.DE56bjSu.K', 'ROLE_ADMIN', 'admin');
