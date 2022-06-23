048a80ea4440b7be5842f8357cabbf8107cd8fd0
IhorBeztsennyi <coalenergy84@ukr.net>
2022-06-23 16:24:55 +0300

    Rollback

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (id, email, password, user_role, username)
 VALUES
     ('7d7e6e35-f7c9-40a1-9efd-a1c5c963d919', 'admin@gmail.com','$2a$12$Ymy2NsY2mm72faISck74Uesp0nRRHwm/LkmFuCU7A8.DE56bjSu.K', 'ROLE_ADMIN', 'admin'),
-    ('8d8e6e35-f7c9-40a1-9efd-a1c5c963d919', 'user@gmail.com','$2a$12$Ymy2NsY2mm72faISck74Uesp0nRRHwm/LkmFuCU7A8.DE56bjSu.K', 'ROLE_USER', 'user1');
+    ('8a7e6e35-f7c9-40a1-9efd-a1c5c963d919', 'user@gmail.com','$2a$12$Ymy2NsY2mm72faISck74Uesp0nRRHwm/LkmFuCU7A8.DE56bjSu.K', 'ROLE_USER', 'user1');
