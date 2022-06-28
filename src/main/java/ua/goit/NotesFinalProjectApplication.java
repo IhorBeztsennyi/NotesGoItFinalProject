01449e1111d234e4b7ac24f5a485b4558cb4dde0
IhorBeztsennyi <coalenergy84@ukr.net>
2022-06-28 23:05:42 +0300

    extends SpringBootServletInitializer

--- src/main/java/ua/goit/NotesFinalProjectApplication.java
+++ src/main/java/ua/goit/NotesFinalProjectApplication.java
@@ -2,11 +2,12 @@ package ua.goit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
-public class NotesFinalProjectApplication {
+public class NotesFinalProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NotesFinalProjectApplication.class, args);
