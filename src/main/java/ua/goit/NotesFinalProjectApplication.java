70a9138b3a94b8a9fe0aefb731086ec29aefaaaa
IhorBeztsennyi <coalenergy84@ukr.net>
2022-06-28 20:23:44 +0300

    @PropertySource("classpath:application.properties")

--- src/main/java/ua/goit/NotesFinalProjectApplication.java
+++ src/main/java/ua/goit/NotesFinalProjectApplication.java
@@ -2,8 +2,10 @@ package ua.goit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class NotesFinalProjectApplication {
 
     public static void main(String[] args) {
